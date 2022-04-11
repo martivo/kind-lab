c773b09af673bc87d3101faac2920808671e0f49
martivo <martin.vool@entigo.com>
2022-04-11 13:15:42 +0300

    Get password before adding apps of app

--- up.sh
+++ up.sh
@@ -14,10 +14,6 @@ kubectl --context kind-test create ns argocd && \
 kubectl --context kind-prod create ns argocd && \
   helm template argocd-yaml/argocd/ | kubectl --context kind-prod apply -n argocd -f- #https://argo-cd.readthedocs.io/en/stable/getting_started/ and https://argo-cd.readthedocs.io/en/stable/operator-manual/installation/
 
-#Create app of apps in argocd
-helm template argocd-yaml/app-of-apps/ --set runenv="test" --set internaldomain="argocd-$1.learn.entigo.io" --set externaldomain="kind-$1.learn.entigo.io" --set iprange="172.18.0.200-172.18.0.209" | kubectl --context kind-test apply -n argocd -f-
-helm template argocd-yaml/app-of-apps/ --set runenv="prod" --set internaldomain="argocd-$1.learn.entigo.io" --set externaldomain="kind-$1.learn.entigo.io" --set iprange="172.18.0.210-172.18.0.219" | kubectl --context kind-prod apply -n argocd -f-
-
 #Wait for clusters to be ready and print argocd login info.
 kind get clusters | while read cluster
 do
@@ -32,3 +28,6 @@ do
 
 done
 
+#Create app of apps in argocd
+helm template argocd-yaml/app-of-apps/ --set runenv="test" --set internaldomain="argocd-$1.learn.entigo.io" --set externaldomain="kind-$1.learn.entigo.io" --set iprange="172.18.0.200-172.18.0.209" | kubectl --context kind-test apply -n argocd -f-
+helm template argocd-yaml/app-of-apps/ --set runenv="prod" --set internaldomain="argocd-$1.learn.entigo.io" --set externaldomain="kind-$1.learn.entigo.io" --set iprange="172.18.0.210-172.18.0.219" | kubectl --context kind-prod apply -n argocd -f-
