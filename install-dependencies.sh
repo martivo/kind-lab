a6c7d0628d6d652ae4440b8acede525024e28495
martivo <martin.vool@entigo.com>
2022-04-11 13:05:39 +0300

    Add proper IP ranges.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+#https://kind.sigs.k8s.io/docs/user/quick-start/#installing-from-release-binaries
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind && mv ./kind /usr/bin/kind
 
+#https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-kubectl-binary-with-curl-on-linux
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl && mv kubectl /usr/bin/kubectl
 
-wget -O kubeseal.tar.gz https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.17.2/kubeseal-0.17.2-linux-amd64.tar.gz
-tar xvzf kubeseal.tar.gz && mv kubeseal /usr/bin/kubeseal
+#https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-bash-linux/
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
 
+#https://helm.sh/docs/intro/install/#from-script
 wget -O helm.tar.gz https://get.helm.sh/helm-v3.8.0-rc.1-linux-amd64.tar.gz
 tar xvzf helm.tar.gz && mv linux-amd64/helm /usr/bin/helm
-
 
 wget -O kubectl-neat.tar.gz https://github.com/itaysk/kubectl-neat/releases/download/v2.0.3/kubectl-neat_darwin_amd64.tar.gz
 tar xvzf kubectl-neat.tar.gz && mv kubectl-neat /usr/bin/kubectl-neat 
--- up.sh
+++ up.sh
@@ -15,22 +15,20 @@ kubectl --context kind-prod create ns argocd && \
   helm template argocd-yaml/argocd/ | kubectl --context kind-prod apply -n argocd -f- #https://argo-cd.readthedocs.io/en/stable/getting_started/ and https://argo-cd.readthedocs.io/en/stable/operator-manual/installation/
 
 #Create app of apps in argocd
-helm template argocd-yaml/app-of-apps/ --set runenv="test" --set internaldomain="argocd-$1.learn.entigo.io" --set externaldomain="kind-$1.learn.entigo.io" --set iprange="10.250.1.200-10.250.1.209" | kubectl --context kind-test apply -n argocd -f-
-helm template argocd-yaml/app-of-apps/ --set runenv="prod" --set internaldomain="argocd-$1.learn.entigo.io" --set externaldomain="kind-$1.learn.entigo.io" --set iprange="10.250.1.210-10.250.1.219" | kubectl --context kind-prod apply -n argocd -f-
+helm template argocd-yaml/app-of-apps/ --set runenv="test" --set internaldomain="argocd-$1.learn.entigo.io" --set externaldomain="kind-$1.learn.entigo.io" --set iprange="172.18.0.200-172.18.0.209" | kubectl --context kind-test apply -n argocd -f-
+helm template argocd-yaml/app-of-apps/ --set runenv="prod" --set internaldomain="argocd-$1.learn.entigo.io" --set externaldomain="kind-$1.learn.entigo.io" --set iprange="172.18.0.210-172.18.0.219" | kubectl --context kind-prod apply -n argocd -f-
 
-
-i=0
+#Wait for clusters to be ready and print argocd login info.
 kind get clusters | while read cluster
 do
-  echo "Waiting for argocd $cluster"
+  echo "Waiting for argocd on cluster $cluster"
   while [ 1 -lt 2 ]
   do
 	kubectl --context kind-$cluster -n argocd get secret argocd-initial-admin-secret && break
 	sleep 10
   done
-  echo -n "Argocd password for $cluster is "
+  echo -n "Argocd user is admin and password for $cluster is "
   kubectl --context kind-$cluster -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-  let i++
-  echo "##################"
+
 done
 
