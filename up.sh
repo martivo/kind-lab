9eca35386529bc007c950f79a61d36db8cbcee27
martivo <martin.vool@entigo.com>
2022-04-20 09:43:20 +0300

    Add loop to wait for loadbalancer service

--- up.sh
+++ up.sh
@@ -38,12 +38,26 @@ done
 helm template argocd-yaml/app-of-apps/ --set runenv="test" --set number=$1 | kubectl --context kind-test apply -n argocd -f-
 helm template argocd-yaml/app-of-apps/ --set runenv="prod" --set number=$1 | kubectl --context kind-prod apply -n argocd -f-
 
-#Suuname internetist tulevad 80 ja 443 pordid prod kobara MetalLB IP peale.
-prod_ip=$(kubectl --context=kind-prod get svc -n haproxy haproxy-ingress --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+
+#Suuname internetist tuleva 80 ja 8080 pordid prod ja test kobara HAproxy teenuse IP peale.
+prod_ip=""
+while [ $prod_ip == "" ]
+do
+  prod_ip=$(kubectl --context=kind-prod get svc -n haproxy haproxy-ingress --output jsonpath='{.status.loadBalancer.ingress[0].ip}')  
+  echo "Waiting for prod cluster HAproxy loadbalancer service."
+  sleep 10
+done
+
 sudo iptables -I FORWARD -p tcp -d $prod_ip --match multiport --dports 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -t nat -I PREROUTING -p tcp -i ens5 --dport 80 -j DNAT --to-destination $prod_ip:80
 
-#Suuname internetist tulevad 8080 ja 8443 pordid prod kobara MetalLB IP peale.
-test_ip=$(kubectl --context=kind-test get svc -n haproxy haproxy-ingress --output jsonpath='{.status.loadBalancer.ingress[0].ip}')
+test_ip=""
+while [ $prod_ip == "" ]
+do
+  test_ip=$(kubectl --context=kind-test get svc -n haproxy haproxy-ingress --output jsonpath='{.status.loadBalancer.ingress[0].ip}')  
+  echo "Waiting for prod cluster HAproxy loadbalancer service."
+  sleep 10
+done
 sudo iptables -I FORWARD -p tcp -d $test_ip --match multiport --dports 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -t nat -I PREROUTING -p tcp -i ens5 --dport 8080 -j DNAT --to-destination $test_ip:80
