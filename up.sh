35d875ef4fff8f95ee1062fafc5a1f05ea44e9b9
martivo <martin.vool@entigo.com>
2022-04-20 10:28:46 +0300

    Fix waiting loop

--- up.sh
+++ up.sh
@@ -53,7 +53,7 @@ sudo iptables -I FORWARD -p tcp -d $prod_ip --match multiport --dports 80 -m sta
 sudo iptables -t nat -I PREROUTING -p tcp -i ens5 --dport 80 -j DNAT --to-destination $prod_ip:80
 
 test_ip=""
-while [ "$prod_ip" == "" ]
+while [ "$test_ip" == "" ]
 do
   test_ip=$(kubectl --context=kind-test get svc -n haproxy haproxy-ingress --output jsonpath='{.status.loadBalancer.ingress[0].ip}')  
   echo "Waiting for prod cluster HAproxy loadbalancer service."
